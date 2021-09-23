1f0cdb59bcd5cd7ba75862fd763993397091ebed
linusericsson <lericsson22@gmail.com>
2021-09-23 12:36:12 +0100

    multi-augment on smae batch

--- comclr_transform.py
+++ comclr_transform.py
@@ -8,6 +8,34 @@ sys.path.append('../elasticdeform/')
 import elasticdeform
 
 
+# for y1, y2 in loader: # batch size 512, meaning effective batch size of 2048
+#    y1_abc, y2_abc = augmentation_for_central(y1, y2) # unconditional augmentations if possible
+#    y1_ab, y2_ab = augmentation_for_ab(y1, y2)
+#    y1_ac, y2_ac = augmentation_for_ac(y1, y2)
+#    y1_bc, y2_bc = augmentation_for_bc(y1, y2)
+
+#    h1, h2 = backbone(y1), backbone(y2)
+#    z1_abc, z2_abc = central_head(h1), central_head(h2)
+#    z1_ab, z2_ab = spatial_head(h1), spatial_head(h2)
+#    z1_ac, z2_ac = colour_head(h1), colour_head(h2)
+#    z1_bc, z2_bc = shape_head(h1), shape_head(h2)
+
+#    loss = barlowtwins(z1_abc, z2_abc) # apply central_bn
+#    loss += barlowtwins(z1_ab, z2_ab) # apply spatial_bn
+#    loss += barlowtwins(z1_ac, z2_ac) # apply colour_bn
+#    loss += barlowtwins(z1_bc, z2_bc) # apply shape_bn
+
+#    # redundancy is on_diag of barlowtwins
+#    loss += redundancy(z1_ab, z1_abc) + redundancy(z2_ab, z2_abc)
+#    loss += redundancy(z1_ac, z1_abc) + redundancy(z2_ac, z2_abc)
+#    loss += redundancy(z1_bc, z1_abc) + redundancy(z2_bc, z2_abc)
+
+#    loss.backward()
+#    optimizer.step()
+
+
+
+
 class ComCLRTransform(object):
     def __init__(self, resize=256, crop_size=224, norm=[[0.485, 0.456, 0.406], [0.229, 0.224, 0.225]]):
         augmentation_dict = {
@@ -61,12 +89,29 @@ class ComCLRTransform(object):
             transforms.Normalize(*norm)
         ])
 
+        self.sections = {
+            'central': [1, 1, 1],
+            'spatial': [0, 1, 1],
+            'colour': [1, 0, 1],
+            'shape': [1, 1, 0],
+        }
+
     def sample(self):
         samples = torch.zeros(len(self.augs))
         probs = torch.ones(len(self.augs)) * 0.5
         while sum(samples) == 0:
             samples = torch.bernoulli(probs)
         return samples
+
+    def sample_for_section(self, union):
+        union = torch.tensor(union)
+        probs = torch.ones(len(self.augs)) * 0.5
+        samples_1 = torch.bernoulli(probs) * union
+        samples_2 = torch.bernoulli(probs) * union
+        while samples_1.sum() == 0:
+            samples_1 = torch.bernoulli(probs) * union
+
+        return samples_1, samples_2
 
     def augment(self, image, samples):
         #print(self.pre_idxs, self.crop_idxs, self.pil_idxs, self.post_idxs)
@@ -101,14 +146,22 @@ class ComCLRTransform(object):
                 #print(f'Applied {self.augs[i]}')
         return image
 
-    def __call__(self, image):
-        samples_1 = self.sample()
-        samples_2 = self.sample()
+    def __call__(self, image, section):
+        image = transforms.functional.to_pil_image(image)
 
+        samples_1, samples_2 = self.sample_for_section(self.sections[section])
         image_1 = self.augment(image.copy(), samples_1)
         image_2 = self.augment(image.copy(), samples_2)
 
-        return image_1, image_2, samples_1, samples_2
+        return image_1, image_2#, samples_1, samples_2
+
+    def augment_batch(self, x, section):
+        y1s, y2s = [], []
+        for _x in x:
+            y1, y2 = self.__call__(_x, section)
+            y1s.append(y1)
+            y2s.append(y2)
+        return torch.stack(y1s), torch.stack(y2s)
 
 
 class SaltAndPepperNoise(object):
